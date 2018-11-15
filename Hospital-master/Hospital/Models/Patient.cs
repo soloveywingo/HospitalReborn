c7df52d6cf7d1697296e4bd694e2cdd292a86519
Bohdan Solovyov <solovey2wingo@mail.ru>
2018-11-15 15:51:05 +0200

    validation for patientsNames

--- Hospital-master/Hospital/Models/Patient.cs
+++ Hospital-master/Hospital/Models/Patient.cs
@@ -1,6 +1,7 @@
 ﻿using Hospital.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -10,19 +11,20 @@ namespace Hospital.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Please enter ur name!")]
+        [MaxLength(30, ErrorMessage = "Isn`t it to much 4 ya?")]
         public string Name { get; set; }
-       
+        
         public Status Status  { get; set; }
 
         public DateTime DayOfBirth { get; set; }
         
         public string TaxCode { get; set; }
-
+        
         public int AttendingDoctorId { get; set; }
 
         public List<Doctor> Doctors { get; set; } = new List<Doctor>();
 
         public string ImageUrl { get; set; }
-
     }
 }
